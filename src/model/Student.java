60e4668b72d9f2e6234313136157f125dbea7639
oreoo2602 <nthoreoo2602@gmail.com>
2021-09-27 21:37:49 +0700

    Build classes: Vaccine, Student, Injection

---
+++ src/model/Student.java
@@ -0,0 +1,34 @@
+package model;
+
+public class Student {
+    
+    private String studentId;
+    private String studentName;
+
+    public Student() {
+        
+    }
+
+    public Student(String studentId, String studentName) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+    }
+
+    public String getStudentId() {
+        return this.studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getStudentName() {
+        return this.studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+
+}
--- src/model/Vaccine.java
+++ src/model/Vaccine.java
@@ -1,5 +1,34 @@
 package model;
 
 public class Vaccine {
-    String id;
+    private String vaccineId;
+    private String vaccineName;
+
+    public Vaccine(String vaccineId, String vaccineName) {
+        this.vaccineId = vaccineId;
+        this.vaccineName = vaccineName;
+    }
+
+    public Vaccine() {
+        
+    }
+
+    public String getVaccineId() {
+        return this.vaccineId;
+    }
+
+    public void setVaccineId(String vaccineId) {
+        this.vaccineId = vaccineId;
+    }
+
+    public String getVaccineName() {
+        return this.vaccineName;
+    }
+
+    public void setVaccineName(String vaccineName) {
+        this.vaccineName = vaccineName;
+    }
+
+
+
 }
